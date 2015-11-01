0ac43c617a2bf59d6b17ec73ca7fba31481ff127
lefreire <letfreirefigueiredo@gmail.com>
2015-10-31 23:26:07 -0200

    Fixed

--- index.js
+++ index.js
@@ -8,36 +8,35 @@ var line, numberLines;
 var countFiles = 0;
 
 
-getInfo(function(lines, numberFiles){
-    startDataBase(function(err, collection){
-       if(err) console.log(err);
-       else{
-           
-            console.log("Numero de linhas: "+ numberFiles);
-           
-            
+startDataBase(function(err, collection){
+    if(err) console.log(err);
+    else{
+    
+       
+       getInfo(function(lines, numberFiles){
+        
+        
+        
+        
             numberLines = lines.length;
             console.log("Numero de linhas: " + numberLines);
             
-            var batch = collection.initializeUnorderedBulkOp();
-            
             for(line = 0 ; line < numberLines; line++){
                 prepareData(lines[line], function(response){   
-                    //saveToDataBase(response, collection, function(err){
-                        batch.insert({response:1});
-                        //if(err) console.log(err);
-                        //else // console.log("["+ response.sign + "] Saved")
-                        
-                    //});
+                   // console.log(response);
+                    saveToDataBase(response, collection, function(err){
+                        if(err) console.log(err);
+                        else{
+                           // console.log("["+ response.sign + "] Saved")
+                        }
+                    });
                 });
-                //if(line === numberLines-1) process.exit();
-            }
-             batch.execute(function(err, result) {
-	  	        console.log(result);
-	         })   
-           }
-       })    
-      
+            }  
+            if(line === numberLines){console.log(line ); countFiles++; console.log(countFiles);}
+            if(countFiles === numberFiles) process.exit(); 
+        })
+    }    
+
 })
 
 
