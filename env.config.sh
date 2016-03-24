6479ce337aef5a4e10791200c4f8639babacc6cc
Mario Cecchi <macecchi@gmail.com>
2016-03-23 23:04:08 -0300

    Remove Docker helper scripts

--- env.config.sh
+++ env.config.sh
@@ -1,5 +1,5 @@
 export RIOBUS_DB_NAME="riobus";
-export RIOBUS_DB_HOST="localjost";
+export RIOBUS_DB_HOST="localhost";
 export RIOBUS_DB_USER="";
 export RIOBUS_DB_PASS="";
 export RIOBUS_DB_PORT="27017";
